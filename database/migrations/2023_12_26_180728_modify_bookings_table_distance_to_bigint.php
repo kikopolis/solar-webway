4e5de24c7be0390673fd94f57a28235112055ca8
Kristo Leas <kristo.leas@gmail.com>
2023-12-26 20:08:55 +0200

    "added migration"

--- database/migrations/2023_12_26_180728_modify_bookings_table_distance_to_bigint.php
+++ database/migrations/2023_12_26_180728_modify_bookings_table_distance_to_bigint.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        Schema::table('bigint', function (Blueprint $table) {
+        Schema::table('bookings', function (Blueprint $table) {
             $table->bigInteger('total_distance')->change();
         });
     }
     
     public function down(): void {
-        Schema::table('bigint', function (Blueprint $table) {
+        Schema::table('bookings', function (Blueprint $table) {
             $table->integer('total_distance')->change();
         });
     }
